7674dfa8cfaf2666ca03adba6f8cb4aa7c603b46
Nithin S Varrier <nithin486@hotmail.com>
2022-08-28 13:33:51 +0530

    fix: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            if (id.includes("@mui")) {
+              return "vendor_mui";
+            } else if (id.includes("firebase")) {
+              return "vendor_firebase";
+            }
+            return "vendor"; // all other package goes here
+          }
+        },
+      },
+    },
+  },
+  plugins: [react(), splitVendorChunkPlugin()],
+});

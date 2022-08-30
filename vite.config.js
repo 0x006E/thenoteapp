aed9d4c8466407a3269c92ab732cee194d148bee
Nithin S Varrier <nithin486@hotmail.com>
2022-08-30 19:57:57 +0530

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,5 @@ import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules"))
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-        },
-      },
-    },
-  },
-  resolve: {
-    alias: [
-      {
-        find: /^@mui\/icons-material\/(.*)/,
-        replacement: "@mui/icons-material/esm/$1",
-      },
-    ],
-  },
   plugins: [react(), splitVendorChunkPlugin()],
 });
